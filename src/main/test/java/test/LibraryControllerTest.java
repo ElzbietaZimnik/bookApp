bee1d117c5ef69f49e2e7881b91f10432ecc58d8
Ela <elik@>
2020-02-24 10:59:05 +0100

    end1

---
+++ src/main/test/java/test/LibraryControllerTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import app.Books;
+import app.Library;
+import app.LibraryController;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class LibraryControllerTest {
+List<Books> books;
+    Library library;
+
+    @Test
+    public void largestNumberOfPages() {
+
+//        List l = new ArrayList<>();
+//        l.add(libraryController.largestNumberOfPages("5","5","5"))
+
+
+    }
+
+
+
+    @Test
+    public void sortBookByReleaseYear() {
+
+
+    }
+
+
+    @Test
+    public void searchByType() {
+
+    }
+
+
+    @Test
+    public void searchByIsbn() {
+
+
+    }
+
+
+    @Test
+    public void searchByAuthor() {
+
+
+    }
+
+}
